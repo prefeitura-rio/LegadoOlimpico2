ea6177f279aa71ab81874fcf3c293d65d75a376d
Lucas Tavares <lucastavarestt@gmail.com>
2024-04-01 10:24:36 -0300

    fix

--- docs/_app/pages/index.svelte-de7dd899.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/vendor-52a36e74.js";import{R as p,p as c}from"../chunks/index-a69dd880.js";/* empty css                                                     */import"../chunks/preload-helper-fcc3e5e3.js";export{p as default,c as prerender};
