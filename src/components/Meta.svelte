3a6f4b1d51fbb2ac1a20b5cc40e8bf4f37308bb7
Lucas Tavares <lucastavarestt@gmail.com>
2024-05-03 11:50:51 -0300

    fix thumb

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -13,21 +13,30 @@
   <meta name="author" content="Legado Olímpico" />
   <meta name="news_keywords" content="" />
 
-  <meta property="og:title" content={title} />
+  <!-- <meta property="og:title" content={title} />
   <meta property="og:site_name" content="Legado Olímpico" />
   <meta property="og:url" content={url} />
   <meta property="og:description" content={description} />
   <meta property="og:type" content="article" />
-  <meta property="og:locale" content="en_US" />
+  <meta property="og:locale" content="pt_BR" />
 
   <meta property="og:image" content="{url}/assets/capa.jpg" />
   <meta property="og:image:type" content="image/jpeg" />
   <meta property="og:image:width" content="1200" />
-  <meta property="og:image:height" content="600" />
+  <meta property="og:image:height" content="600" /> -->
 
-  <meta name="twitter:title" content={title} />
+  <meta property="og:image" content="{url}/assets/images/capa.jpg" />
+	<meta property="og:image:type" content="image/jpeg" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="600" />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="https://dados.rio/" />
+	<meta name="twitter:creator" content="@escritoriodedadosviz" />
+	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image:src" content="{url}/assets/capa.jpg" />
+	<meta name="twitter:image:src" content="{url}/assets/images/capa.jpg" />
+
 
   <meta name="robots" content="max-image-preview:large" />
 
